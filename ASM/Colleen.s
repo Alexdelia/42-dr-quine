c47a8b37025396748c42b622d402ef206f660dd5
Alexdelia <alexandre.delille.57@gmail.com>
2022-02-09 16:25:14 +0100

    same in ASM, got a lot of struggle but it work now

---
+++ ASM/Colleen.s
@@ -0,0 +1,18 @@
+; 42
+
+global main
+extern printf
+
+section .text
+main:
+push rbx
+lea rdi, [rel foo]
+mov rsi, 10
+mov rdx, 34
+lea rcx, [rel foo]
+call printf ; 42
+pop rsi
+ret
+
+foo:
+db "; 42%1$c%1$cglobal main%1$cextern printf%1$c%1$csection .text%1$cmain:%1$cpush rbx%1$clea rdi, [rel foo]%1$cmov rsi, 10%1$cmov rdx, 34%1$clea rcx, [rel foo]%1$ccall printf ; 42%1$cpop rsi%1$cret%1$c%1$cfoo:%1$cdb %2$c%3$s%2$c, 0%1$c", 0
