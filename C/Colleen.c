b4b7fab997e7b0c84e0f4fed944e776afe05c593
Alexdelia <alexandre.delille.57@gmail.com>
2022-02-09 15:01:05 +0100

    finish Colleen.c

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 // 42
-char *foo(void)
+
+char	*foo(void)
 {
-	return "#include <stdio.h>%1%c";
+	return "#include <stdio.h>%1$c%1$c// 42%1$c%1$cchar%2$c*foo(void)%1$c{%1$c%2$creturn %3$c%4$s%3$c;%1$c}%1$c%1$cint%2$cmain(void)%1$c{%1$c%2$c// 42%1$c%2$cchar%2$c*a = foo();%1$c%2$cprintf(a, 10, 9, 34, a);%1$c}%1$c";
 }
-int main(void)
+
+int	main(void)
 {
 	// 42
 	char	*a = foo();
