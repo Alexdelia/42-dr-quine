4e1b66a5d1e6d0640b9635403fcbe3a93566ba65
Alexdelia <alexandre.delille.57@gmail.com>
2022-02-09 15:02:44 +0100

    add parenthese :3

--- C/Colleen.c
+++ C/Colleen.c
@@ -4,7 +4,7 @@
 
 char	*foo(void)
 {
-	return "#include <stdio.h>%1$c%1$c// 42%1$c%1$cchar%2$c*foo(void)%1$c{%1$c%2$creturn %3$c%4$s%3$c;%1$c}%1$c%1$cint%2$cmain(void)%1$c{%1$c%2$c// 42%1$c%2$cchar%2$c*a = foo();%1$c%2$cprintf(a, 10, 9, 34, a);%1$c}%1$c";
+	return ("#include <stdio.h>%1$c%1$c// 42%1$c%1$cchar%2$c*foo(void)%1$c{%1$c%2$creturn (%3$c%4$s%3$c);%1$c}%1$c%1$cint%2$cmain(void)%1$c{%1$c%2$c// 42%1$c%2$cchar%2$c*a = foo();%1$c%2$cprintf(a, 10, 9, 34, a);%1$c}%1$c");
 }
 
 int	main(void)
