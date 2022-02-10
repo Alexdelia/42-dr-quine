884c9a203ee69c16084cbb1cfc0c83ecc58eeadc
Alexdelia <alexandre.delille.57@gmail.com>
2022-02-10 12:37:54 +0100

    change comment to be different

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,4 +1,4 @@
-; 42
+; 42 outside
 
 global main
 extern printf
@@ -10,9 +10,9 @@ lea rdi, [rel foo]
 mov rsi, 10
 mov rdx, 34
 lea rcx, [rel foo]
-call printf ; 42
+call printf ; 42 inside
 pop rsi
 ret
 
 foo:
-db "; 42%1$c%1$cglobal main%1$cextern printf%1$c%1$csection .text%1$cmain:%1$cpush rbx%1$clea rdi, [rel foo]%1$cmov rsi, 10%1$cmov rdx, 34%1$clea rcx, [rel foo]%1$ccall printf ; 42%1$cpop rsi%1$cret%1$c%1$cfoo:%1$cdb %2$c%3$s%2$c, 0%1$c", 0
+db "; 42 outside%1$c%1$cglobal main%1$cextern printf%1$c%1$csection .text%1$cmain:%1$cpush rbx%1$clea rdi, [rel foo]%1$cmov rsi, 10%1$cmov rdx, 34%1$clea rcx, [rel foo]%1$ccall printf ; 42 inside%1$cpop rsi%1$cret%1$c%1$cfoo:%1$cdb %2$c%3$s%2$c, 0%1$c", 0
--- C/Colleen.c
+++ C/Colleen.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-// 42
+// 42 outside
 
 char	*foo(void)
 {
-	return ("#include <stdio.h>%1$c%1$c// 42%1$c%1$cchar%2$c*foo(void)%1$c{%1$c%2$creturn (%3$c%4$s%3$c);%1$c}%1$c%1$cint%2$cmain(void)%1$c{%1$c%2$c// 42%1$c%2$cchar%2$c*a = foo();%1$c%2$cprintf(a, 10, 9, 34, a);%1$c}%1$c");
+	return ("#include <stdio.h>%1$c%1$c// 42 outside%1$c%1$cchar%2$c*foo(void)%1$c{%1$c%2$creturn (%3$c%4$s%3$c);%1$c}%1$c%1$cint%2$cmain(void)%1$c{%1$c%2$c// 42 inside%1$c%2$cchar%2$c*a = foo();%1$c%2$cprintf(a, 10, 9, 34, a);%1$c}%1$c");
 }
 
 int	main(void)
 {
-	// 42
+	// 42 inside
 	char	*a = foo();
 	printf(a, 10, 9, 34, a);
 }
