12a5563188648c43ae482a040957f56304457822
adelille <adelille@student.42.fr>
2022-10-02 20:00:30 +0200

    finished `Grace.py`

--- Python/Grace.py
+++ Python/Grace.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 file = open("Grace_kid.py", "w")
-code = '#!/usr/bin/env python3\n\nfile = open("Grace_kid.py", "w")\ncode = %r\nfile.write(code%%code)\nfile.close()'
-ft = lambda x: file.write(x%x); file.close()
+code = '#!/usr/bin/env python3\n\nfile = open("Grace_kid.py", "w")\ncode = %r\nft = lambda x: file.write(x%%x) and file.close()\n\n# 42\n\nft(code)\n'
+ft = lambda x: file.write(x%x) and file.close()
 
 # 42
 
