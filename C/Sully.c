8f40d9ef3ed26d51b57303d1c0c1031e597326fc
Alexdelia <alexandre.delille.57@gmail.com>
2022-10-09 18:13:45 +0200

    fix for C and Python i < 0

--- C/Sully.c
+++ C/Sully.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define CODE	"#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <unistd.h>%1$c%1$c#define CODE%2$c%3$c%5$s%3$c%1$c%1$cint%2$cmain(void)%1$c{%1$c%2$cint%2$c%2$ci = %4$d;%1$c%2$cchar%2$cexe[16];%1$c%2$cchar%2$cfile[16];%1$c%2$cFILE%2$c*fd;%1$c%2$cchar%2$ccmd[100];%1$c%1$c%2$cif (!i)%1$c%2$c%2$creturn (0);%1$c%2$cif (access(%3$cSully_5.c%3$c, R_OK) != -1)%1$c%2$c%2$ci--;%1$c%2$csprintf(file, %3$cSully_%%d.c%3$c, i);%1$c%2$csprintf(exe, %3$cSully_%%d%3$c, i);%1$c%2$cfd = fopen(file, %3$cw%3$c);%1$c%2$cfprintf(fd, CODE, 10, 9, 34, i, CODE);%1$c%2$cfclose(fd);%1$c%2$csprintf(cmd, %3$cclang %%s -o %%s && ./%%s%3$c, file, exe, exe);%1$c%2$csystem(cmd);%1$c%2$creturn (0);%1$c}%1$c"
+#define CODE	"#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <unistd.h>%1$c%1$c#define CODE%2$c%3$c%5$s%3$c%1$c%1$cint%2$cmain(void)%1$c{%1$c%2$cint%2$c%2$ci = %4$d;%1$c%2$cchar%2$cexe[16];%1$c%2$cchar%2$cfile[16];%1$c%2$cFILE%2$c*fd;%1$c%2$cchar%2$ccmd[100];%1$c%1$c%2$cif (i <= 0)%1$c%2$c%2$creturn (0);%1$c%2$cif (access(%3$cSully_5.c%3$c, R_OK) != -1)%1$c%2$c%2$ci--;%1$c%2$csprintf(file, %3$cSully_%%d.c%3$c, i);%1$c%2$csprintf(exe, %3$cSully_%%d%3$c, i);%1$c%2$cfd = fopen(file, %3$cw%3$c);%1$c%2$cfprintf(fd, CODE, 10, 9, 34, i, CODE);%1$c%2$cfclose(fd);%1$c%2$csprintf(cmd, %3$cclang %%s -o %%s && ./%%s%3$c, file, exe, exe);%1$c%2$csystem(cmd);%1$c%2$creturn (0);%1$c}%1$c"
 
 int	main(void)
 {
@@ -12,7 +12,7 @@ int	main(void)
 	FILE	*fd;
 	char	cmd[100];
 
-	if (!i)
+	if (i <= 0)
 		return (0);
 	if (access("Sully_5.c", R_OK) != -1)
 		i--;
--- Python/Sully.py
+++ Python/Sully.py
@@ -4,14 +4,14 @@ from os import F_OK, access
 from subprocess import call
 
 i = 5
-if i == 0:
+if i <= 0:
     exit()
 
 if access("Sully_5.py", F_OK):
     i -= 1
 
 file = open("Sully_%d.py" % i, "w+")
-code = '#!/usr/bin/env python3\n\nfrom os import F_OK, access\nfrom subprocess import call\n\ni = %d\nif i == 0:\n    exit()\n\nif access("Sully_5.py", F_OK):\n    i -= 1\n\nfile = open("Sully_%%d.py" %% i, "w+")\ncode = %r\n\nfile.write(code %% (i, code))\nfile.close()\n\ncall(["python3", "Sully_%%d.py" %% i])\n'
+code = '#!/usr/bin/env python3\n\nfrom os import F_OK, access\nfrom subprocess import call\n\ni = %d\nif i <= 0:\n    exit()\n\nif access("Sully_5.py", F_OK):\n    i -= 1\n\nfile = open("Sully_%%d.py" %% i, "w+")\ncode = %r\n\nfile.write(code %% (i, code))\nfile.close()\n\ncall(["python3", "Sully_%%d.py" %% i])\n'
 
 file.write(code % (i, code))
 file.close()
